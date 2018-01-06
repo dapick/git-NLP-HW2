b149a72a0953387e6167336f85ec6b25b2b12a42
dblaus <dblaus@users.noreply.github.com>
2018-01-06 10:15:41 +0200

    output files

---
+++ chu_liu_wrapper.py
@@ -0,0 +1,28 @@
+from consts import Consts
+
+class Chu_Liu_Wrapper:
+
+    def __init__(self, sentences):
+
+        self.sentences = sentences
+        self.sentences_klicks = self.init_klicks()
+
+
+    def init_klicks(self):
+
+        list_of_klicks = []
+        for idx, sentence in enumerate(self.sentences):
+            list_of_klicks.append({})
+            list_of_klicks[idx][0] = list(range(1, len(sentence['words'])))
+            for i in range(1, len(sentence['words'])):
+                list_of_klicks[idx][i] = list((range(1, i))) + list(range(i + 1, len(sentence['words'])))
+
+
+        return list_of_klicks
+
+    def print_klick_to_file(self):
+        with open('../data_from_training/kilck', 'w+') as f:
+            for idx, sentence in enumerate(self.sentences_klicks):
+                f.write(str(idx) + " =>\n")
+                for key, value in sentence.items():
+                    f.write("\t" + str(key) + " => " + str(value) + "\n")
