89d22366db31dfb4802e4acfa997bb61b1a61a25
Daniella Pick <danielapick@gmail.com>
2018-01-06 11:26:05 +0200

    Renamed tests to its right name

--- tests/featuresTest.py
+++ tests/featuresTest.py
@@ -9,6 +9,11 @@ class FeaturesTestCase(unittest.TestCase):
         feature = Features(Consts.TRAIN, Consts.BASIC_MODEL, Consts.FEATURE_LIST_BASIC, Consts.PATH_TO_TRAINING_FROM_TEST)
         feature.print_features_to_file()
 
+    def test_match_features_to_hm(self):
+        feature = Features(Consts.LABEL, Consts.BASIC_MODEL, Consts.FEATURE_LIST_BASIC,
+                           Consts.PATH_TO_TRAINING_FROM_TEST)
+        print(feature.get_features_idx_per_h_m(0, (2, 7)))
+
 
 if __name__ == '__main__':
     unittest.main()
