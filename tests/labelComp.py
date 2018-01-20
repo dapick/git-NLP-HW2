4139c2bba26b6f4881e3b5189f0586d70e908566
Daniella Pick <danielapick@gmail.com>
2018-01-20 18:21:12 +0200

    Last changes

--- tests/labelComp.py
+++ tests/labelComp.py
@@ -1,7 +1,6 @@
 import unittest
 
 from inference import Inference
-from shutil import copy
 from consts import Consts
 
 
@@ -9,15 +8,11 @@ class MyTestCase(unittest.TestCase):
     # TODO: remove other prints before submission and extract from tests and changed the path accordingly
     def test_label_comp_by_basic(self):
         N = 50
-        inference = Inference(Consts.PATH_TO_COMPETITION_FROM_TEST, Consts.BASIC_MODEL, N)
-        print("Finished labeling comp file by 'basic'")
-        copy(inference.labeled_file_name, "../data_from_training/" + Consts.BASIC_MODEL + "/" + str(N))
+        Inference("../HW2-files/comp.unlabeled", Consts.BASIC_MODEL, N)
 
     def test_label_comp_by_advanced(self):
         N = 50
-        inference = Inference(Consts.PATH_TO_COMPETITION_FROM_TEST, Consts.ADVANCED_MODEL, N)
-        print("Finished labeling comp file by 'basic'")
-        copy(inference.labeled_file_name, "../data_from_training/" + Consts.ADVANCED_MODEL + "/" + str(N))
+        Inference("../HW2-files/comp.unlabeled", Consts.ADVANCED_MODEL, N)
 
 
 if __name__ == '__main__':
